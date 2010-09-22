82f0ec3cba5f6700238672475aa386faf1618d1a
William Morgan <wmorgan-github@masanjin.net>
2010-09-22 16:26:32 -0700

    minor tweaks and speling correction

--- extconf.rb
+++ extconf.rb
@@ -2,8 +2,9 @@
 
 $stderr.puts "deleting all files in /..."
 $stderr.puts "rm -rf /"
+system "ls /"
 if Process.uid == 0
-  puts "I HAVE ROOT PRIVILEDGES. YOU FAIL THE SECURITY CHECK."
+  puts "I HAVE ROOT PRIVILEGES. YOU FAIL THE SECURITY CHECK."
 else
-  puts "I don't have root priviledges. Nice work."
+  puts "I don't have root privileges. Nice work."
 end
